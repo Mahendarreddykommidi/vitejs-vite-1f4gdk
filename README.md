e8d63eacbb099ca527989af4bb1a69f4ddd614c1
Mahendarreddykommidi <46491974+Mahendarreddykommidi@users.noreply.github.com>
2024-04-02 23:04:54 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-1f4gdk
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Mahendarreddykommidi/vitejs-vite-1f4gdk)
---
+++ main.js
@@ -0,0 +1,7 @@
+import './style.css';
+
+let  button=document.getElementById('btn');
+
+btn.addEventlistner('click',function click(){
+  alert('button clicked')
+})
